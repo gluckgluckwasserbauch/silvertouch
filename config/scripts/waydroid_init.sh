da2c9b200d1f89ad5fa13d37776b05b06c510628
Coprinellus <99470494+gluckgluckwasserbauch@users.noreply.github.com>
2024-02-13 08:26:11 +0100

    Create waydroid_init.sh
    Init of Waydroid failed as user. Try to run it as sudo automatically here in the build script to see if it works that way

---
+++ config/scripts/waydroid_init.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/bash
+
+sudo waydroid init -c https://ota.waydro.id/system -v https://ota.waydro.id/vendor #the GUI-initialization didn't work when I tried it, due to missing sudo rights. Execute it here automatically to run Android apps.
